e6f86a73e4c8c2d1fd0ccfb5a6e89ad8e6b85088
James Mariano <jam829@drexel.edu>
2018-05-26 21:59:10 -0400

    Start

---
+++ app/src/main/java/edu/drexel/jam829/finalproject/MainActivity.java
@@ -0,0 +1,15 @@
+// Application designed by James Mariano and Olis ______
+
+package edu.drexel.jam829.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
